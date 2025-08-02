3f563d3ad39e51ebeead0c57683f5cde0ef9a8df
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-08-02 18:36:21 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+//ESERCIZI SUGLI OGGETTI
+
+/* Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+ */
+
+const biciclette = [
+    {
+        nome: 'bici_1',
+        peso: 3
+        
+    },
+    {
+        nome: 'bici_2',
+        peso: 5
+        
+    },
+    {
+        nome: 'bici_3',
+        peso: 30
+        
+    },
+    {
+        nome: 'bici_4',
+        peso: 2
+        
+    }
+]
+
+
+/*Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+
+const squadre = [
+  {
+    name: 'Juventus',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Inter',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Milan',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Fiorentina',
+    points: 0,
+    fouls: 0
+  }
+]
